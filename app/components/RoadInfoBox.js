9daaa3f3256a28f88de672dd5e670d1f29c2dd48
nearHaan <farhaan.nizam675@gmail.com>
2025-07-04 00:34:11 +0530

    First commit

---
+++ app/components/RoadInfoBox.js
@@ -0,0 +1,37 @@
+export default function RoadInfoBox({
+    roadid,
+    roadName,
+    roadLen,
+    munci,
+    panch,
+    block,
+    width,
+    surfaceType,
+    soilType
+}) {
+    return (
+        <div className="mt-2 w-fit flex-col items-center bg-white rounded-2xl text-black shadow-md min-w-[200] overflow-hidden max-w-[250]">
+            <div className="p-2 bg-[#F0F2F5] bg-clip-border"><label className="text-black">Road Information</label></div>
+            <div className="p-2 grid grid grid-cols-[80px_1fr] gap-1 items-center place-items-start truncate">
+                <label className="w-full text-xs text-[#7B7B7B]">Road id</label>
+                <label className="w-full text-sm text-black truncate" title={roadid}>{roadid ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Road Name</label>
+                <label className="w-full text-sm text-black truncate" title="">{roadName ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Road Length</label>
+                <label className="w-full text-sm text-black truncate" title="">{roadLen ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Muncipality</label>
+                <label className="w-full text-sm text-black truncate" title="">{munci ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Panchayat</label>
+                <label className="w-full text-sm text-black truncate" title="">{panch ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Block</label>
+                <label className="w-full text-sm text-black truncate" title="">{block ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Width</label>
+                <label className="w-full text-sm text-black truncate" title="">{width ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Surface Type</label>
+                <label className="w-full text-sm text-black truncate" title="">{surfaceType ?? '--'}</label>
+                <label className="w-full text-xs text-[#7B7B7B]">Soil Type</label>
+                <label className="w-full text-sm text-black truncate" title="">{soilType ?? '--'}</label>
+            </div>
+        </div>
+    );
+}
