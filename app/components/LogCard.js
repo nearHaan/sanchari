c39bc610828193fccf678d65f2849313add57d0c
nearHaan <farhaan.nizam675@gmail.com>
2025-07-07 14:40:53 +0530

    Add Log Click functionality

--- app/components/LogCard.js
+++ app/components/LogCard.js
@@ -4,7 +4,7 @@ export default function LogCard({timestamp, name, desc, onClick}) {
     return (
         <div className="mx-2 mt-2 p-2 border-black border-1 rounded-lg h-fit">
             <div>
-                <label className="text-black text-sm font-bold">{timestamp}</label>
+                <label className="text-black text-sm font-bold">{new Date(timestamp).toLocaleString()}</label>
                 <div className="my-1 flex items-center">
                     <div className="mr-2 p-1 bg-[#E5E8EB] rounded-full">
                         < AvatarIcon />
@@ -12,9 +12,9 @@ export default function LogCard({timestamp, name, desc, onClick}) {
                     <label className="text-black text-sm">{name}</label>
                 </div>
                 <label className="text-black text-sm">{desc}</label>
-                <div className="flex items-center justify-end text-[#1A3FE5] text-xs font-bold">
+                <button onClick={onClick} className="flex items-center justify-end text-[#1A3FE5] text-xs font-bold">
                     Show in map
-                </div>
+                </button>
             </div>
         </div>
     );
