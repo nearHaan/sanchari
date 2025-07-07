76ec6c0a1aba0b719ee815a8dcb18f4e0e66a74b
nearHaan <farhaan.nizam675@gmail.com>
2025-07-07 15:06:50 +0530

    Change UI

--- app/components/LogCard.js
+++ app/components/LogCard.js
@@ -2,11 +2,11 @@ import { AvatarIcon } from "./Icons";
 
 export default function LogCard({timestamp, name, desc, onClick}) {
     return (
-        <div className="mx-2 mt-2 p-2 border-black border-1 rounded-lg h-fit">
+        <div className="mx-2 mt-4 p-2 bg-white shadow-lg drop-shadow rounded-lg h-fit">
             <div>
                 <label className="text-black text-sm font-bold">{new Date(timestamp).toLocaleString()}</label>
                 <div className="my-1 flex items-center">
-                    <div className="mr-2 p-1 bg-[#E5E8EB] rounded-full">
+                    <div className="mr-2 p-1 bg-[#F5F5F5] rounded-full">
                         < AvatarIcon />
                     </div>
                     <label className="text-black text-sm">{name}</label>
