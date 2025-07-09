82ec5567027e14b76554bd9eaca6e0acdc132855
nearHaan <farhaan.nizam675@gmail.com>
2025-07-09 23:09:40 +0530

    Change UI

--- app/components/LockedRoadTile.js
+++ app/components/LockedRoadTile.js
@@ -5,7 +5,7 @@ export default function LockedRoadTile({ roadid, state, onClick }) {
         <tr className="odd:bg-[#F6F6F6] even:bg-[#E5E8EB]">
             <td className="text-left px-2 py-2">{roadid}</td>
             <td className="place-items-center px-2 py-2">
-                <div className={`w-2 h-2 rounded-full ${state == 'c'?"bg-yellow-300":"bg-green-500"}`}></div>
+                <div className={`w-2 h-2 rounded-full border-[1px] border-white ${state == 'c'?"bg-yellow-300":"bg-green-500"}`}></div>
             </td>
             <td className="place-items-center px-2 py-2">
                 <button onClick={() => onClick(roadid)} className="flex place-items-center cursor-pointer hover:scale-105">< LockOpen/></button>
